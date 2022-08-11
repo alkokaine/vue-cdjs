6007add8014cb21b40cf18e8cb0ed044556155c7
Алексей Коковин <KokovinAA@cross-d.ru>
2022-08-11 16:01:15 +0300

    lastMonthDay, prevMonthDays

--- src/common/month-days.js
+++ src/common/month-days.js
@@ -3,6 +3,18 @@ import axios from 'axios'
 
 const daysInMonth = (year, month) => (moment(`${year}-${month}-01`, 'YYYY-MM')).daysInMonth()
 const createDate = (year, month, day) => (moment(`${year}-${month}-${day}`, 'YYYY-MM-DD'))
+const lastMonthDay = (year, month) => (createDate(year, month, daysInMonth(year, month)))
+const prevMonthDays = (date) => {
+  const prevmonth = (moment(date)).subtract(1, 'month')
+  const lastDay = lastMonthDay(prevmonth.year(), prevmonth.month() + 1)
+  const days = []
+  while (lastDay.day() > 0) {
+    days.unshift({ date: moment(lastDay.toDate()), isprev: true })
+    lastDay.subtract(1, 'days')
+  }
+  return days
+}
+
 const getDays = (date, options = { pre: 1, covid: 1, sd: 0 }) => {
   const days = []
   const params = {
@@ -26,32 +38,7 @@ const getDays = (date, options = { pre: 1, covid: 1, sd: 0 }) => {
       days.push(...Array.from(Array(daysInMonth(params.year, params.month)).keys())
         .map((m, index) => ({ date: createDate(params.year, params.month, index + 1) })))
     }).finally(() => {
-      try {
-        const prevmonth = (moment(date)).subtract(1, 'month')
-        const _prepend = []
-        const prevmonth_year = prevmonth.year()
-        const prevmonth_month = prevmonth.month() > 10 ? prevmonth.month() + 1 : `0${prevmonth.month() + 1}`
-        const prevmonth_days = daysInMonth(prevmonth_year, prevmonth_month)
-        const _lastDay = createDate(prevmonth_year, prevmonth_month, prevmonth_days)
-        while (_lastDay.day() !== 1) {
-          _prepend.unshift({ date: _lastDay, isprev: true})
-          _lastDay.subtract(1, 'days')
-        }
-        // const fd = createDate(params.year, params.month, days[0].date.getDay())
-        // const dayOfWeek = fd.day()
-        // if (dayOfWeek === 1) resolve(days)
-        // let ln = dayOfWeek - 1
-        // const result = []
-        // while (ln > 0) {
-        //   const d = fd.subtract(1, 'days')
-        //   result.unshift({ date: d, isprev: true })
-        //   ln -= 1
-        // }
-        return resolve(_prepend.concat(days))
-      }
-      catch (err) {
-        return reject(err)
-      }
+      resolve(days)
     })
   })
 }
@@ -59,5 +46,6 @@ const getDays = (date, options = { pre: 1, covid: 1, sd: 0 }) => {
 export {
   createDate,
   daysInMonth,
+  prevMonthDays,
   getDays
 }
