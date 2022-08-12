2c12dafbd20dfa169e28b8dc5833fb58105fa501
sit19 <alkokaine@yandex.ru>
2022-08-12 07:19:49 +0300

    prevMonthWeekLength

--- src/common/month-days.js
+++ src/common/month-days.js
@@ -4,6 +4,10 @@ import axios from 'axios'
 const daysInMonth = (year, month) => (moment(`${year}-${month}-01`, 'YYYY-MM')).daysInMonth()
 const createDate = (year, month, day) => (moment(`${year}-${month}-${day}`, 'YYYY-MM-DD'))
 const lastMonthDay = (year, month) => (createDate(year, month, daysInMonth(year, month)))
+const prevMonthWeekLength = (date) => {
+  const prev = (moment(date)).subtract(1, 'month')
+  return (lastMonthDay(prev.year(), prev.month() + 1)).day()
+}
 const prevMonthDays = (date) => {
   const prevmonth = (moment(date)).subtract(1, 'month')
   const lastDay = lastMonthDay(prevmonth.year(), prevmonth.month() + 1)
@@ -14,7 +18,6 @@ const prevMonthDays = (date) => {
   }
   return days
 }
-
 const getDays = (date, options = { pre: 1, covid: 1, sd: 0 }) => {
   const days = []
   const params = {
@@ -42,10 +45,19 @@ const getDays = (date, options = { pre: 1, covid: 1, sd: 0 }) => {
     })
   })
 }
-
+const weekdays = (
+  [1, 2, 3, 4, 5, 6, 7]
+    .map(d => ({
+      weekday: moment().isoWeekday(d),
+      order: d,
+      day: (d - 1)
+    })
+))
 export {
   createDate,
   daysInMonth,
   prevMonthDays,
+  prevMonthWeekLength,
+  weekdays,
   getDays
 }
