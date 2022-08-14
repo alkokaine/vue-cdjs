cc579fea59375b15c4b625f563a93d860ec303a7
sit19 <alkokaine@yandex.ru>
2022-08-14 18:21:58 +0300

    cd-day: begin

---
+++ src/components/cd-day.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="cd-day--block">
+    <div class="cd-day--header">
+      <div class="cd-day--number">{{ date.getDate() }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    date: { type: Date, required: true }
+  }
+}
+</script>
+
+<style>
+
+</style>
