d84c2d56a872e9152822a925e00ed635d4d85b34
sit19 <alkokaine@yandex.ru>
2022-08-14 17:24:14 +0300

    dev-views

---
+++ src/views/list-view.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="list-view">A</div>
+</template>
+
+<script>
+export default {
+    name: 'list-view',
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/month-view.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="month">
+    <el-date-picker v-model="currentdate" format="yyyy-MM-dd"></el-date-picker>
+    <cd-month :date="currentdate"></cd-month>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CDMonth from '@/components/cd-month.vue'
+import { DatePicker } from 'element-ui'
+export default {
+  name: 'MonthView',
+  components: {
+    'cd-month': CDMonth,
+    'el-date-picker': DatePicker
+  },
+  data () {
+    return {
+      currentdate: new Date(Date.now())
+    }
+  }
+}
+</script>
+<style>
+</style>
